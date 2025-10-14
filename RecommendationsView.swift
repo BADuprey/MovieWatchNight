afbb0389e57d083c642cea4f6700683b6d672d97
Sasha Jazmin Abuin <111884777+Sabuin@users.noreply.github.com>
2025-10-14 13:26:48 -0400

    Recommendations_view; non-functional

---
+++ RecommendationsView.swift
@@ -0,0 +1,24 @@
+//
+//  RecommendationsView.swift
+//  sprint_1
+//
+//  Created by Sasha Jazmin Abuin on 10/12/25.
+//
+
+//View for recommendations
+
+import SwiftUI
+
+struct RecommendationsView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Movie recommendations (placeholder)")
+                .padding()
+                .navigationTitle("Recommendations")
+        }
+    }
+}
+
+#Preview {
+    RecommendationsView()
+}
