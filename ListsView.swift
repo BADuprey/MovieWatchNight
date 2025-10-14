9432c2cb3d412598c224eb2262e655f86a1d7663
Sasha Jazmin Abuin <111884777+Sabuin@users.noreply.github.com>
2025-10-14 13:25:57 -0400

    View for lists, non-functional

---
+++ ListsView.swift
@@ -0,0 +1,24 @@
+//
+//  ListsView.swift
+//  sprint_1
+//
+//  Created by Sasha Jazmin Abuin on 10/12/25.
+//
+
+//View for lists
+
+import SwiftUI
+
+struct ListsView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Your lists (placeholder)")
+                .padding()
+                .navigationTitle("Lists")
+        }
+    }
+}
+
+#Preview {
+    ListsView()
+}
