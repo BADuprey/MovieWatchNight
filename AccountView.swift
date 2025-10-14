78197876406ca9119f1c0995c826b276bb61ad5c
Sasha Jazmin Abuin <111884777+Sabuin@users.noreply.github.com>
2025-10-14 13:29:20 -0400

    View for user's account settings; non-functional

---
+++ AccountView.swift
@@ -0,0 +1,22 @@
+//
+//  AccountView.swift
+//  sprint_1
+//
+//  Created by Sasha Jazmin Abuin on 10/12/25.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Account info (placeholder)")
+                .padding()
+                .navigationTitle("Account")
+        }
+    }
+}
+
+#Preview {
+    AccountView()
+}
