1be9abe6db939054e0184ef5f66ed22f9f817136
Sasha Jazmin Abuin <111884777+Sabuin@users.noreply.github.com>
2025-10-14 13:27:56 -0400

    View for schedules (merged); non-functional

---
+++ SchedulesView.swift
@@ -0,0 +1,23 @@
+//
+//  SchedulesView.swift
+//  sprint_1
+//
+//  Created by Sasha Jazmin Abuin on 10/12/25.
+//
+
+//View for schedules
+import SwiftUI
+
+struct SchedulesView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Merged schedules (placeholder)")
+                .padding()
+                .navigationTitle("Schedules")
+        }
+    }
+}
+
+#Preview {
+    SchedulesView()
+}
